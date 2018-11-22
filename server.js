0ca3a21b07768c7a6e8c4a1498fbeefd86519cfe
Anukoon <anukoon1996@gmail.com>
2018-11-22 15:17:13 +0700

    Test Push

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-//app.use(requireHTTPS);
+
 app.use(express.static(path.join(__dirname, 'Eterna')));
 
 // app.get('/*', function(req, res){
